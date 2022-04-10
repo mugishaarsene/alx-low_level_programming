c01475966a341c7ff7a9b4d8e48fa9e4667df755
mugishaarsene <a.mugisha@alustudent.com>
2022-04-10 05:57:09 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * My hello, printf task
- * Return : return 0 after program execution
+ * Return: return 1 after program execution
  */
 int main(void)
-[
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-]
+}
