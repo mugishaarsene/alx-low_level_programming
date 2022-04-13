5409a9a9868d4217ef2c5e84aad43057778c3765
mugishaarsene <a.mugisha@alustudent.com>
2022-04-13 12:27:31 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,6 @@ int main(void)
 	{
 		putchar(lower_case);
 	}
-
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
 		putchar(upper_case);
@@ -22,5 +21,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-
 }
