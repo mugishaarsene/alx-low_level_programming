301738564f85a8a96192711ce6eff6b8f2b37be2
mugishaarsene <a.mugisha@alustudent.com>
2022-04-13 12:46:08 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,25 +2,25 @@
 /**
  * main -Entry point
  *
- * Return: Always 0 (Success/correct)
+ * Return : Always 0 (Success/correct)
  */
 int main(void)
 {
 	char lower_case;
 	char upper_case;
 
-for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-{
-	putchar(lower_case);
-}
+	for (upper_case = 'A'; upper_case <= "Z"; upper_case++)
+	{
+		putchar(upper_case);
+	}
 
-for (upper_case = "A"; upper_case <= "Z"; upper_case++)
-{
-	putchar(upper_case);
-}
-	
-putchar ('\n');
+	for (lower_case = "a"; lower_case <= "z"; lower_case++)
+	{
+		putchar(lower_case);
+	}
 
-return (0);
+	putchar ('\n');
+
+	return (0);
 
 }
