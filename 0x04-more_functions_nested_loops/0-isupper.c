15f8169d4eef4b9d55a7dbe060a6362d089a42b7
mugishaarsene <a.mugisha@alustudent.com>
2022-04-14 15:10:09 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,20 +2,20 @@
 /**
  *  _isupper - evalue if letter is uppercase .
  * @c: print int
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1):
-	}
+if (c >= 'A' && c <= 'Z')
+   {
+	return (1);
+   }
 
-	else
-	{
-		return (0):
-	}
+else
+   {
+	return (0);
+   }
 
 }
 
