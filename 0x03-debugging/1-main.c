b1a28ad51a820e6c2def11fbe70b022ad5df22c6
mugishaarsene <a.mugisha@alustudent.com>
2022-04-14 17:41:59 -0700

    task 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - causes on infinite loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :('\n');
+
+i = 0;
+
+/*
+ *while (i < 10)
+ *{
+ *Infinite 1000 - No increase of variable
+ *putchar(1);
+}
+*/
+
+printf("Infinite loop  avoided! \\o/\n");
+return (0);
+}
