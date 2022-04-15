6603ede870605674726a2c32b2d566d7b2e45eec
mugishaarsene <a.mugisha@alustudent.com>
2022-04-14 18:05:08 -0700

    task 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,18 +4,16 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-i = 0;
-
+	int 0;
 
 /*
- *while (i < 10)
+*while (i < 10)
  *{
  *Infinite loop - No increase of variable
  *putchar(i);
