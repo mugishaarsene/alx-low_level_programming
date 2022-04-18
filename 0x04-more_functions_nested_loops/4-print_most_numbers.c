be9602b6fd0ed20c93b0225e26cb23b6182439cf
mugishaarsene <a.mugisha@alustudent.com>
2022-04-17 23:34:54 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,13 @@ void print_most_numbers(void)
 int i;
 
 i = 0;
-while (i < 10)
+while (i < 10; i++)
 {
 	if (i != 2 && i != 4)
 {
-_putchar (i + '0');
+	_putchar (i + '0');
 }
-i++;
 }
 
 _putchar ('\n');
-return (0);
 }
