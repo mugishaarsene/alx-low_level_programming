a84cac8649f3316c0768c74fa59d39aa915ffdf4
mugishaarsene <a.mugisha@alustudent.com>
2022-04-17 22:51:12 -0700

    task 5

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>>
 /**
  * print_numbers - prints the number zero to nine
  *
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,12 @@
 void print_most_numbers(void)
 {
 
-	int i;
+int i;
+
 i = 0;
 while (i < 10)
 {
-if (i != 2 && i != 4)
+	if (i != 2 && i != 4)
 {
 _putchar (i + '0');
 }
